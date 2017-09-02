05ba3fffca85152c3d5b67a995a57c4056513f9b
vinitm <my_name_is97@yahoo.com>
2017-09-02 20:43:11 +0530

    add support for images

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ var request = require('request');
 var config = require('./config');
 var app = express();
 
-app.get(['/api/v2/list_movies.json', '/ajax/search'], function(req, res) {
+app.get(['/api/v2/list_movies.json', '/ajax/search', '/assets/images/movies/*'], function(req, res) {
 	var url = config.baseUrl + req.originalUrl;
 	request.get(url)
 		.on('error', function(err) {
 			console.log(err);
 		}).pipe(res);
 });
-
 
 app.get('/*', function(req, res) {
 	res.status(403);
